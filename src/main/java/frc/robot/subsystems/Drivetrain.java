f22b0b443a93caaceb52e7c4e5b03e4ecc4ca66b
Paochis <al02956220@tecmilenio.mx>
2023-01-10 10:50:07 -0600

    xboxController

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,6 +8,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import frc.robot.Constants.DrivetrainConstants;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.ExampleCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class Drivetrain extends SubsystemBase {
   
@@ -22,6 +28,19 @@ private final CANSparkMax m_frontRight = new CANSparkMax(DrivetrainConstants.kMo
     
 private final CANSparkMax m_rearRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[3],
     CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    private final CommandXboxController m_driverController =
+    new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+    private void configureBindings() {
+      // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+      new Trigger(m_exampleSubsystem::exampleCondition)
+          .onTrue(new ExampleCommand(m_exampleSubsystem));
+  
+      // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+      // cancelling on release.
+      m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    }
 
     
   @Override
